7331136b3a5265e231874aa726dd9649bb0d5054
Ravishka Dissanayaka <133754606+RND036@users.noreply.github.com>
2024-08-04 13:55:46 +0530

    Revert "Login And Register"

--- resources/views/components/authentication-card-logo.blade.php
+++
@@ -1,3 +0,0 @@
-<a href="/">
-   <img src="assets/img/logo/Newlogo.png" alt="" srcset="">
-</a>
--- routes/web.php
+++ routes/web.php
@@ -26,13 +26,3 @@ Route::get('/Product', function () {
 Route::get('/checkout', function () {
     return view('Page.checkout');
 });
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified',
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
